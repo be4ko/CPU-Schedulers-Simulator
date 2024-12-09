477e772ad406b943559943572442044a29cc663a
be4ko <11410120210733@stud.cu.edu.eg>
2024-12-09 07:30:42 +0200

    Fixing Sleep

--- src/FCAI/FCAISchedule.java
+++ src/FCAI/FCAISchedule.java
@@ -83,7 +83,7 @@ public class FCAISchedule {
                     }
 
                     time++;
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                     process.updateBurstTime(1);
 
                     System.out.println("Executing " + process.process.name + ", remaining burst time: "
@@ -133,7 +133,7 @@ public class FCAISchedule {
         for (int i = 0; i < processes.length - 1; i++) {
             scheduleThread(processes[i]);
             try {
-                Thread.sleep((processes[i + 1].process.arrivalTime - processes[i].process.arrivalTime) * 100);
+                Thread.sleep((processes[i + 1].process.arrivalTime - processes[i].process.arrivalTime) * 1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
---
+++ src/Priority_GUI.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class Priority_GUI {
+    private JTextField textField1;
+}
